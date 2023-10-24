32a713dd7b703ecb09b6f7d3acdcaa01406c5673
qMaxFcq <maxzazafc123@gmail.com>
2023-10-24 20:18:15 +0700

    done add real db

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -49,14 +49,14 @@ exports.addNewOrder = async (req, res) => {
   // console.log(req.user[0].username);
   try {
     const { side, symbol, price, amount, shop_id, customer } = req.body;
-    const db_test = await connectToDatabaseTest();
+    const db_test = await connectToDatabase();
     const currentDate = new Date();
     const exchange_order_id = formatDate(currentDate);
     const cost = amount * price;
     const order_status = "COMPLETED";
 
     const sql =
-      "INSERT INTO order_temp (shop_id, side, symbol, price, amount, cost, customer, exchange_order_id, order_status, add_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      "INSERT INTO `order` (shop_id, side, symbol, price, amount, cost, customer, exchange_order_id, order_status, add_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     const [result] = await db_test.query(sql, [
       shop_id,
       side,
@@ -80,9 +80,9 @@ exports.addNewOrder = async (req, res) => {
 exports.editOrder = async (req, res) => {
   try {
     const orderId = req.params.id;
-    const db_test = await connectToDatabaseTest();
+    const db_test = await connectToDatabase();
     const [existingOrder] = await db_test.query(
-      "SELECT * FROM order_temp WHERE id = ?",
+      "SELECT * FROM `order` WHERE id = ?",
       [orderId]
     );
 
@@ -95,7 +95,7 @@ exports.editOrder = async (req, res) => {
 
     // ดึงค่า completed_at ปัจจุบัน
     const [currentCompletedAt] = await db_test.query(
-      "SELECT completed_at FROM order_temp WHERE id = ?",
+      "SELECT completed_at FROM `order` WHERE id = ?",
       [orderId]
     );
 
@@ -106,7 +106,7 @@ exports.editOrder = async (req, res) => {
 
       // อัปเดตค่า completed_at ในฐานข้อมูล
       await db_test.query(
-        "UPDATE order_temp SET shop_id=?, side=?, symbol=?, price=?, amount=?, cost=?, customer=?, completed_at=? , edit_by=? WHERE id = ?",
+        "UPDATE `order` SET shop_id=?, side=?, symbol=?, price=?, amount=?, cost=?, customer=?, completed_at=? , edit_by=? WHERE id = ?",
 
         [
           shop_id,
@@ -131,99 +131,6 @@ exports.editOrder = async (req, res) => {
   }
 };
 
-// exports.getOrderHistory = async (req, res) => {
-//   try {
-//     let selectedDate = req.query.selectedDate;
-//     if (selectedDate) {
-//       selectedDate = selectedDate.split("/").reverse().join("-");
-//     } else {
-//       selectedDate = new Date().toISOString().split("T")[0];
-//     }
-
-//     const limit = 10;
-//     let page = parseInt(req.query.page) || 1;
-//     page = isNaN(page) || page < 1 ? 1 : page;
-//     const offset = (page - 1) * limit;
-
-//     const db_test = await connectToDatabaseTest();
-//     const [rows] = await db_test.execute(
-//       `SELECT * FROM order_temp WHERE DATE(created_time) = ? LIMIT ${limit} OFFSET ${offset}`,
-//       [selectedDate]
-//     );
-
-//     db_test.end();
-//     res.json({ data: rows });
-//   } catch (error) {
-//     console.error("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
-//     res.status(500).json({
-//       success: false,
-//       error: "เกิดข้อผิดพลาดในการดึงข้อมูล getOrderHistory",
-//     });
-//   }
-// };
-
-// exports.getWithdrawDepositAllCoin = async (req, res) => {
-//   try {
-//     let selectedDate = req.query.selectedDate;
-//     if (selectedDate) {
-//       selectedDate = selectedDate.split("/").reverse().join("-");
-//     } else {
-//       selectedDate = new Date().toISOString().split("T")[0];
-//     }
-
-//     console.log(selectedDate);
-
-//     const db_test = await connectToDatabaseTest();
-//     const [allHistory] = await db_test.execute(
-//       "SELECT * FROM order_temp WHERE DATE(created_time) = ?",
-//       [selectedDate]
-//     );
-//     db_test.end();
-
-//     // สร้างออบเจกต์เพื่อเก็บยอดรวมแยกตามเหรียญและฝ่าย
-//     const totalsByCoin = {
-//       USDT_THB: { SELL: 0, BUY: 0 },
-//       BTC_THB: { SELL: 0, BUY: 0 },
-//       ETH_THB: { SELL: 0, BUY: 0 },
-//       BNB_THB: { SELL: 0, BUY: 0 },
-//     };
-
-//     allHistory.forEach((record) => {
-//       const isSellSymbol =
-//         ["USDT_THB", "BTC_THB", "ETH_THB", "BNB_THB"].includes(record.symbol) &&
-//         record.side === "SELL";
-//       const isBuySymbol =
-//         ["USDT_THB", "BTC_THB", "ETH_THB", "BNB_THB"].includes(record.symbol) &&
-//         record.side === "BUY";
-
-//       if (isSellSymbol || isBuySymbol) {
-//         const coin = record.symbol;
-//         const amount = parseFloat(record.amount);
-
-//         if (record.side === "SELL") {
-//           totalsByCoin[coin].SELL += amount;
-//         } else if (record.side === "BUY") {
-//           totalsByCoin[coin].BUY += amount;
-//         }
-//       }
-//     });
-
-//     const db = await connectToDatabase();
-//     const [rows_wow] = await db.execute(
-//       "SELECT * FROM tally_wid_depo WHERE DATE(created_time) = ?",
-//       [selectedDate]
-//     );
-//     db.end();
-
-//     res.status(200).json({ totalsByCoin, rows_wow });
-//   } catch (error) {
-//     console.error("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
-//     res.status(500).json({
-//       success: false,
-//       error: "เกิดข้อผิดพลาดในการดึงข้อมูล getWithdrawDepositAllCoin",
-//     });
-//   }
-// };
 
 exports.getOrderHistory = async (req, res) => {
   try {
@@ -239,17 +146,17 @@ exports.getOrderHistory = async (req, res) => {
     page = isNaN(page) || page < 1 ? 1 : page;
     const offset = (page - 1) * limit;
 
-    const db_test = await connectToDatabaseTest();
+    const db_test = await connectToDatabase();
 
     // ดึงประวัติออเดอร์ทั้งหมด
     const [orderHistory] = await db_test.execute(
-      `SELECT * FROM order_temp WHERE DATE(created_time) = ? LIMIT ${limit} OFFSET ${offset}`,
+      `SELECT * FROM \`order\` WHERE DATE(created_time) = ? LIMIT ${limit} OFFSET ${offset}`,
       [selectedDate]
     );
 
     // ดึงออเดอร์เพื่อคำนวณ amount
     const [withdrawDepositHistory] = await db_test.execute(
-      "SELECT * FROM order_temp WHERE DATE(created_time) = ?",
+      "SELECT * FROM `order` WHERE DATE(created_time) = ?",
       [selectedDate]
     );
 
