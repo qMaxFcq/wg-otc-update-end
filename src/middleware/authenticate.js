af18a0ffd78566219fe5a8ff8a63fccd19645cd0
maxmac <glpat-SFRtQnRyf2AAd73R8BAf>
2024-03-13 17:32:45 +0700

    change connect db in auth

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,16 +2,25 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const mysql = require("mysql2/promise");
 
-async function connectToDatabase() {
-  const db = await mysql.createConnection({
-    host: process.env.DB_HOST_NEW,
-    user: process.env.DB_USERNAME_NEW,
-    password: process.env.DB_PASSWORD_NEW,
-    database: process.env.DB_NAME_NEW,
-  });
+// async function connectToDatabase() {
+//   const db = await mysql.createConnection({
+//     host: process.env.DB_HOST_NEW,
+//     user: process.env.DB_USERNAME_NEW,
+//     password: process.env.DB_PASSWORD_NEW,
+//     database: process.env.DB_NAME_NEW,
+//   });
 
-  return db;
-}
+//   return db;
+// }
+
+const db_test = mysql.createPool({
+  connectionLimit : 5,
+  host: process.env.DB_HOST_NEW,
+  user: process.env.DB_USERNAME_NEW,
+  password: process.env.DB_PASSWORD_NEW,
+  database: process.env.DB_NAME_NEW,
+});
+
 
 module.exports = async (req, res, next) => {
   try {
@@ -22,17 +31,19 @@ module.exports = async (req, res, next) => {
     }
 
     const user = jwt.verify(authToken, process.env.JWT_SECRET_KEY);
-    const db = await connectToDatabase();
-    const userDetail = await db.query(
+    // const db = await connectToDatabase();
+    const userDetail = await db_test.query(
       "SELECT * from user WHERE email = ?",
       user.email
     );
-    await db.close();
 
     req.user = userDetail[0];
     next();
   } catch (err) {
     res.json({ message: "ไม่มี token นะจ่ะ" });
     next(err);
+  } finally {
+    await db_test.releaseConnection();
+
   }
 };
